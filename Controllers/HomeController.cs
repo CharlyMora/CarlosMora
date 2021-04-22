396c3115c8fbad0a4d0ebadbd07f09163076de10
CharlyMora <morandres.carlos@gmail.com>
2021-04-22 17:12:33 -0500

    not found page and route added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace CarlosMora.Controllers
             return View();
         }
 
+        public IActionResult MyNotFound()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace CarlosMora.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string hola ="hola";
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
