4651847f956c50ce4594b72f8570af3265cddaf5
CharlyMora <morandres.carlos@gmail.com>
2021-05-10 21:34:15 -0500

    ready with current pages to inspire

--- Models/InspirationsList.cs
+++ Models/InspirationsList.cs
@@ -5,9 +5,12 @@ namespace CarlosMora.Models
     public class InspirationsList
     {
         public string[,] myArrayOfPages = new string[,]{
-            {"Digit", "Pagina de diseño frances", "https://www.digitz.fr/#/"},
-            {"Name2", "Description2", "Link2"}
+            {"Digit", "Pagina de diseño frances", "https://www.digitz.fr/#/","digitz.fr"},
+            {"Brittany's Chiang Web page", "A good developer web page", "https://brittanychiang.com/", "brittanychiang.com"},
+            {"Riccardo's Zanutta Web page", "Another good developer web page", "http://riccardozanutta.com/", "riccardozanutta.com"}
+
         };
 
     }
 }
+            // {"Name2", "Description2", "Link2", "name2"}
