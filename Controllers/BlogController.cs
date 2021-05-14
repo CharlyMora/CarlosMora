ef25a3b72bdefe01c4d26c6a3af4572f1a3003b5
CharlyMora <morandres.carlos@gmail.com>
2021-05-13 20:06:24 -0500

    done with verification

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -11,15 +11,18 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 
 namespace CarlosMora.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly ICompositeViewEngine _compositeViewEngine;
         private readonly ILogger<BlogController> _logger;
 
-        public BlogController(ILogger<BlogController> logger)
+        public BlogController(ILogger<BlogController> logger, ICompositeViewEngine compositeViewEngine)
         {
+            _compositeViewEngine = compositeViewEngine;
             _logger = logger;
         }
 
@@ -27,7 +30,7 @@ namespace CarlosMora.Controllers
         {
             return View();
         }
-        public async Task<ActionResult>  Index()
+        public async Task<ActionResult> Index()
         {
             var myRepos = await ApiServices.ProcessRepositories2();
             return View(myRepos);
@@ -35,18 +38,23 @@ namespace CarlosMora.Controllers
 
         public IActionResult Entries(string id)
         {
-            if (id==null) return Redirect("Index");
+            if (id == null) return Redirect("Index");
 
-            if (!id.Equals("Entrie1")) {return RedirectToAction("Index","Blog");};
+            //if redirected to Index alone the new route will be Blog/Entries/Index
+            // and this will create a conflict in the browser
+            //if (!id.Equals("Entrie1")) {return RedirectToAction("Index","Blog");};
 
             //validar con lista o base de datos que exista la vista
             //o ver como se pueden gestionar los articulos 
             //teniendo en cuenta que tienen estructuras un poco diferentes en terminos
             //de titulos y listas enumeradas etc
             // por ende no se pueden guardar como un modelo de la misma manera
+            var viewName = $"~/Views/Blog/{id}.cshtml";
+            var result = _compositeViewEngine.GetView("", viewName, false);
 
-            
-            return View(id);
+            if (result.Success) return View(id);
+
+            return RedirectToAction("MyNotFound","Home");
         }
 
 
